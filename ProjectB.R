a37ccc83100234fdd21823888339abc7e4b688e2
Alexandra Ramlogan Salgado <aramlogan02@gmail.com>
2024-11-20 03:00:52 -0500

    Update ProjectB.R

--- ProjectB.R
+++ ProjectB.R
@@ -22,26 +22,53 @@ crPlots(full_model)
 
 ##All of the component + residual plots are approximately linear, and don't need any transformations
 
+
+# Fit a new model with the transformed variable
+full_model <- lm( stressLevel ~ sleepQuality + studyLoad + extracurriculars + I(log(sleepQuality * studyLoad)^2), data = df)
+boxplot(sqrt_model)
+
+crPlots(full_model)
+
+
 #####Residual Analysis
 plot(full_model)
-
+par(mfrow = c(1, 1))
 #look at leverage
-tail(sort(hatvalues(full_model)), n=10) 
+leverageval <- (2 * 4)/520 
+print(leverageval)
+tail(sort(hatvalues(full_model))) 
+leverage <- hatvalues(model)
+violationLeverage <- which(leverage > leverageval)
+print(length(violationLeverage)) #35 in violation!!!
+
 
 #look at cook's distance
 tail(sort(cooks.distance(full_model)), n = 10) # no violation
 
 #look at jackknife residual -> n - k - 2
-t <- qt(.025, 520 - 5 - 2, lower.tail = FALSE)
+t <- qt(.025, 520 - 4 - 2, lower.tail = FALSE)
 print(t)
 
-head(sort(studres(full_model)), n=20) #Print 20 lowest values, 7 in violation
-tail(sort(studres(full_model)), n=20) #Print 20 highest values, 15 in violation
+jackKnife <- sort(studres(full_model)) 
+violationjackKnife <- which(abs(jackKnife) > t)
 
-shapiro.test(full_model$residuals) #not violated -> normality is good
+print(violationjackKnife) ##5 in violation
 
+#jackknife
+head(sort(studres(full_model)), n=20) #Print 20 lowest values, none in violation
+tail(sort(studres(full_model)), n=20) #Print 20 highest values, 5 in violation
+
+#leverage
+print(leverageval)
+tail(sort(hatvalues(full_model)), n=) #Print 10 largest values, 6 in violation
+
+# Shapiro-Wilk Test for Normality
+shapiro.test(full_model$residuals) #violated -> check residuals
+
+boxCox(full_model)
 
 residual_plotting <- function(residuals){
+  
   m <- mean(residuals)
   s <- sd(residuals)
   hist_data <- hist(residuals, breaks=24)
@@ -52,19 +79,30 @@ residual_plotting <- function(residuals){
 }
 residual_plotting(full_model$residuals)
 
-altered_model <- lm(I(stressLevel) ~ ., data = df) #none of the options look good
+
+altered_model <- lm(I(sqrt(stressLevel)) ~ sleepQuality + studyLoad + extracurriculars + I(log(sleepQuality * studyLoad)^2), data = df) #none of the options look good
+boxCox(altered_model)
 
 residual_plotting(altered_model$residuals)
 
-#Co-linearity 
+# Shapiro-Wilk Test for Normality
+shapiro.test(altered_model$residuals) #still in violation, but we will move on anyways
 
+full_model <- altered_model
+
+#Co-linearity 
 options(scipen = 20)
 ols_coll_diag(full_model)
 
-#VIF is ok!
+#variable 5 is in violation, so remove 
+altered_model <- lm(I(sqrt(stressLevel)) ~ sleepQuality + studyLoad + extracurriculars, data = df)
+ols_coll_diag(altered_model)
+
+#VIF is ok, and CI is now ok!
+full_model <- altered_model
 
 #set seed
-set.seed(123)
+set.seed(1)
 full <- df
 
 #Observastion numbers
@@ -82,27 +120,116 @@ print(test_data)
 # Selection Types
 attach(train_data)
 
-final_model <- lm(stressLevel ~ headaches + sleepQuality + studyLoad + extracurriculars + I(headaches * extracurriculars), data = train_data)
-model_all <- ols_step_all_possible(final_model)
-
-print(model_all)
+final_model <- full_model
+model_all <- ols_step_all_possible(full_model)
+head(model_all[order(model_all$adjr, decreasing = T),])
+#plot(model_all)
+head(model_all[order(model_all$aic, decreasing = F),])
 
 #Backwards Selection with p-value as the criteria
 model_backward <- ols_step_backward_p(final_model,prem = 0.1 ,details=TRUE)
 print(model_backward)
+plot(model_backward)
+
+#using AIC
+model_backward2 <- ols_step_backward_aic(full_model,details=TRUE)
+print(model_backward2)
+
+
+######### Forwards Selection
+model_forward <- ols_step_forward_p(full_model, penter = 0.1, details=TRUE)
+print(model_forward)
+
+#Use ols_step_forward_aic to use AIC instead of p-values.
+model_forward_AIC <- ols_step_forward_aic(full_model, details=TRUE)
+print(model_forward_AIC)
+plot(model_forward_AIC)
+
+
+######### Piecewise Selection
+model_step1 <- ols_step_both_p(full_model, pent = 0.1, prem = 0.3, details=T)
+print(model_step1)
+
+model_step2 <- ols_step_both_aic(full_model, details=T)
+print(model_step2)
+
 
 #check reliability and model fit
 df2 <- train_data[,-c(2)]
+df2
 head(df2)
+final_model <- lm(I(sqrt(stressLevel)) ~ studyLoad + sleepQuality, data = df2)
 
-final_model2 <- lm(stressLevel ~ headaches + sleepQuality + studyLoad + extracurriculars + I(headaches * extracurriculars), data = df2)
 summary(final_model)
 
 #y-hats
-pred_train <- predict(final_model2, train_data)
+pred_train <- predict(final_model, train_data)
 mse(train_data$stressLevel, pred_train)
 
-pred_test <- predict(final_model2, test_data)
+pred_test <- predict(final_model, test_data)
 mse(test_data$stressLevel, pred_test)
  
-###train MSE approximately equal test MSE
+###train MSE > test MSE -> overfitting -> reduce training size
+set.seed(123)
+full <- df
+
+#Observastion numbers
+#Using 50% for training
+train=sample(1:nrow(full),nrow(full)*0.50) 
+test=(-train)
+
+#testing and training split
+
+train_data <- full[train, ]
+test_data <- full[test, ]
+print(train_data)
+print(test_data)
+
+# Selection Types
+attach(train_data)
+
+final_model <- full_model
+model_all <- ols_step_all_possible(full_model)
+
+#Backwards Selection with p-value as the criteria
+model_backward <- ols_step_backward_p(final_model,prem = 0.1 ,details=TRUE)
+print(model_backward)
+plot(model_backward)
+
+#using AIC
+model_backward2 <- ols_step_backward_aic(full_model,details=TRUE)
+print(model_backward2)
+
+
+######### Forwards Selection
+model_forward <- ols_step_forward_p(full_model, penter = 0.1, details=TRUE)
+print(model_forward)
+
+#Use ols_step_forward_aic to use AIC instead of p-values.
+model_forward_AIC <- ols_step_forward_aic(full_model, details=TRUE)
+print(model_forward_AIC)
+plot(model_forward_AIC)
+
+
+######### Piecewise Selection
+model_step1 <- ols_step_both_p(full_model, pent = 0.1, prem = 0.3, details=T)
+print(model_step1)
+
+model_step2 <- ols_step_both_aic(full_model, details=T)
+print(model_step2)
+
+
+#check reliability and model fit
+df2 <- train_data[,-c(2)]
+df2
+head(df2)
+final_model <- lm(I(sqrt(stressLevel)) ~ studyLoad + sleepQuality, data = df2)
+
+summary(final_model)
+
+#y-hats
+pred_train <- predict(final_model, train_data)
+mse(train_data$stressLevel, pred_train)
+
+pred_test <- predict(final_model, test_data)
+mse(test_data$stressLevel, pred_test)
